2cb4263c69de48627f64f0d7404b78c6d822947e
keithdimeni <keithdimeni@gmail.com>
2024-03-19 23:39:58 -0300

    mudanca no tipo de variavel feito no exercicio

---
+++ modulo-3/src/exercicio/Exemplo.java
@@ -0,0 +1,9 @@
+package exercicio;
+
+public class Exemplo {
+    public static void main(String[] args) {
+        long num = 10;
+        Integer numI = (int)num;
+        System.out.println(numI);
+    }
+}
