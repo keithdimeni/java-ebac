67ec377f5ef52b7ab2ed995488e10106923d33c9
keithdimeni <keithdimeni@gmail.com>
2024-04-25 00:55:34 -0300

    padrao de projeto singleton

---
+++ modulo-8-padroes-de-projeto/src/br/com/keith/singleton/App.java
@@ -0,0 +1,19 @@
+package br.com.keith.singleton;
+
+public class App {
+    public static void main(String[] args) {
+        Singleton singleton = Singleton.getInstance();
+        Singleton a = Singleton.getInstance();
+
+        System.out.println(singleton);
+        System.out.println(a);
+
+        SingletonPropriedade singletonPropriedade = SingletonPropriedade.getInstance("teste1");
+        SingletonPropriedade singletonPropriedade2 = SingletonPropriedade.getInstance("teste2");
+
+        System.out.println(singletonPropriedade.getValue());
+        System.out.println(singletonPropriedade2.getValue());
+
+
+    }
+}
---
+++ modulo-8-padroes-de-projeto/src/br/com/keith/singleton/SingletonPropriedade.java
@@ -0,0 +1,21 @@
+package br.com.keith.singleton;
+
+public class SingletonPropriedade {
+    private static SingletonPropriedade singletonPropriedade;
+    private String value;
+
+    private SingletonPropriedade(String value){
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public static SingletonPropriedade getInstance(String value) {
+        if (singletonPropriedade == null) {
+            singletonPropriedade = new SingletonPropriedade(value);
+        }
+        return singletonPropriedade;
+    }
+}
